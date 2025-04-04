18f9dd1718cfa4478c8eb01f92f7201aeb1d9d69
jinHanyoon <miracle7532@naver.com>
2025-04-05 00:18:53 +0900

    sss

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import *as http from "http";
 
 const app = express();
 const server = http.createServer(app)
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 const io = new Server(server,{
 //크로스 오리진
     cors:{
