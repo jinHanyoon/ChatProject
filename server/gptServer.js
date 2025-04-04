31b7fd81d39b8f9fb0becb1bc1d89d06f45c3322
jinHanyoon <miracle7532@naver.com>
2025-04-05 01:06:04 +0900

    cors

--- server/gptServer.js
+++ server/gptServer.js
@@ -7,9 +7,12 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST']
+  origin: ['https://chat-project-wheat.vercel.app', 'http://localhost:3000'],  // 클라이언트 도메인 추가
+  methods: ['GET', 'POST', 'OPTIONS'],  // OPTIONS 메서드 추가
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
+app.options('*', cors());
 const PORT = process.env.PORT || 10001;
 
 
